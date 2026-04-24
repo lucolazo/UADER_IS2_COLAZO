a3dbe6afe5fed2e34e9ac9eb6427f4f3d402b70a
Lucolazo <anaacolazo@gmail.com>
2026-04-24 11:35:10 -0300

    sources

---
+++ src/prototype.py
@@ -0,0 +1,32 @@
+#*------------------------------------------------------------------------
+#* Ingeniería de Software II
+#* Patrones de Creación
+#* Prototype
+#* UADER - Ingeniería de Software II
+#* Dr. Pedro E. Colla
+#*------------------------------------------------------------------------
+
+# Cada objeto debe sacer como copiarse a si mismo
+
+class Note(object):
+
+    def __init__(self, fraction):
+        self.fraction = fraction
+
+    def get(self):
+        return self.fraction
+
+    def clone(self):
+        return Note(self.fraction)
+
+    def set(self, fraction):
+        self.fraction = fraction
+
+
+x=Note(10)
+print("Valor almacenado en (x) fraction %d" % (x.get()))
+a=x.clone()
+print("Valor almacenado en (a) fraction %d" % (a.get()))
+
+
+
